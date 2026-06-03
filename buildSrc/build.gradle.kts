4059e0730cb0512915bec9524e53fa6be1d1539f
kqkqku86-a11y <kqkqku86@gmail.com>
2026-06-03 18:34:52 +0700

    Fix: Add missing repositories to buildSrc build.gradle.kts
    - Add google() and mavenCentral() repositories to resolve Android Gradle Plugin dependencies
    - This fixes the compilation error: Could not resolve all files for configuration ':buildSrc:compileClasspath'

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
 	`kotlin-dsl`
 }
 
+repositories {
+	google()
+	mavenCentral()
+	gradlePluginPortal()
+}
+
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
 	implementation("com.android.tools.build:gradle:8.5.0")
@@ -9,8 +15,4 @@ dependencies {
 	implementation("org.openrewrite:plugin:6.19.1")
 	implementation("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:4.2.0")
 	implementation("net.ltgt.nullaway:net.ltgt.nullaway.gradle.plugin:2.3.0")
-}
-
-repositories {
-	gradlePluginPortal()
 }
