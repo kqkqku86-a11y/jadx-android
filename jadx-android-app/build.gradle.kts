9620b4ee3fea4a9408e2fc8b886258854891ae8c
kqkqku86-a11y <kqkqku86@gmail.com>
2026-06-03 19:30:55 +0700

    Fix: Use compilerOptions at android block level instead of kotlinOptions
    The compilerOptions DSL must be used directly in the android block, not nested inside kotlinOptions.

--- jadx-android-app/build.gradle.kts
+++ jadx-android-app/build.gradle.kts
@@ -48,10 +48,8 @@ android {
 		targetCompatibility = JavaVersion.VERSION_11
 	}
 	
-	kotlinOptions {
-		compilerOptions {
-			jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-		}
+	compilerOptions {
+		jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
 	}
 }
 
